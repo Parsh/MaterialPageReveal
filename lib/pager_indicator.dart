a7024ae6459f51ab18b3117514e3b2ab20b1ee93
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-16 20:00:39 +0530

    Parameterizing

--- lib/pager_indicator.dart
+++ lib/pager_indicator.dart
@@ -1,17 +1,31 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'page.dart';
 
 class PagerIndicator extends StatelessWidget {
-
   final PagerIndicatorViewModel pagerIndicatorViewModel;
 
-  PagerIndicator({
-    this.pagerIndicatorViewModel
-  });
+  PagerIndicator({this.pagerIndicatorViewModel});
 
   @override
   Widget build(BuildContext context) {
+
+    List<PageBubble> bubbles = [];
+    for(var i = 0; i < pagerIndicatorViewModel.pages.length; ++i){
+      final page = pagerIndicatorViewModel.pages[i];
+      bubbles.add(
+        new PageBubble(
+          pageBubbleViewModel: new PageBubbleViewModel(
+                    color: page.color,
+                    iconAssetPath: page.iconAssetIcons,
+                    isHollow: true,
+                    activePercent: i == pagerIndicatorViewModel.activeIndex? 1.0: 0.0)
+        )
+      );
+    }
+
     return new Column(
       children: <Widget>[
         new Expanded(
@@ -19,86 +33,83 @@ class PagerIndicator extends StatelessWidget {
         ),
         new Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                width: 20.0,
-                height: 20.0,
-                decoration: new BoxDecoration(
-                    color: new Color(0x88FFFFFF), shape: BoxShape.circle),
-              ),
-            ),
-           new PageBubble(),
-            new Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                width: 20.0,
-                height: 20.0,
-                decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: new Border.all(
-                        color: new Color(0x88FFFFFF), width: 3.0)),
-              ),
-            )
-          ],
+          children: bubbles
+          // children: <Widget>[
+          //  new PageBubble(
+          //       pageBubbleViewModel: new PageBubbleViewModel(
+          //           color: Colors.green,
+          //           iconAssetPath: 'assets/shopping_cart.png',
+          //           isHollow: true,
+          //           activePercent: 0.0)),
+          //   new PageBubble(
+          //       pageBubbleViewModel: new PageBubbleViewModel(
+          //           color: Colors.green,
+          //           iconAssetPath: 'assets/shopping_cart.png',
+          //           isHollow: false,
+          //           activePercent: 1.0)),
+          //   new PageBubble(
+          //       pageBubbleViewModel: new PageBubbleViewModel(
+          //           color: Colors.green,
+          //           iconAssetPath: 'assets/shopping_cart.png',
+          //           isHollow: true,
+          //           activePercent: 0.0))
+          // ],
         )
       ],
     );
   }
 }
 
-
 class PageBubble extends StatelessWidget {
+  final PageBubbleViewModel pageBubbleViewModel;
+
+  PageBubble({this.pageBubbleViewModel});
+
   @override
   Widget build(BuildContext context) {
     return new Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                  width: 45.0,
-                  height: 45.0,
-                  decoration: new BoxDecoration(
-                      color: new Color(0x88FFFFFF), shape: BoxShape.circle),
-                  child: new Image.asset(
-                    'assets/wallet.png',
-                    color: Colors.blue,
-                  )),
-            );
+      padding: const EdgeInsets.all(10.0),
+      child: new Container(
+          width: lerpDouble(20.0, 45.0, pageBubbleViewModel.activePercent),
+          height: lerpDouble(20.0, 45.0, pageBubbleViewModel.activePercent),
+          decoration: new BoxDecoration(
+              color: pageBubbleViewModel.isHollow? Colors.transparent: new Color(0x88FFFFFF), //Showing a fill when not hollow
+              shape: BoxShape.circle,
+              border: new Border.all(
+                color: pageBubbleViewModel.isHollow? new Color(0x88FFFFFF): Colors.transparent, //Showing border when is hollow
+                width: 3.0
+              )            
+              ),
+          child: new Opacity(
+            opacity: pageBubbleViewModel.activePercent,
+            child: new Image.asset(
+              pageBubbleViewModel.iconAssetPath,
+              color: pageBubbleViewModel.color,
+            ),
+          )
+        ), 
+      );
   }
 }
 
+enum SlideDirection { leftToRight, rightToLeft, none }
 
-enum SlideDirection{
-  leftToRight,
-  rightToLeft
-}
-
-class PagerIndicatorViewModel{
+class PagerIndicatorViewModel {
   final List<PageViewModel> pages;
   final int activeIndex;
   final SlideDirection slideDirection;
   final double slidePercent;
 
-
-  PagerIndicatorViewModel({
-    this.pages,
-    this.activeIndex,
-    this.slideDirection,
-    this.slidePercent
-  });
+  PagerIndicatorViewModel(
+      {this.pages, this.activeIndex, this.slideDirection, this.slidePercent});
 }
 
-class PagerBubbleViewModel{
+class PageBubbleViewModel {
   final Color color;
   final String iconAssetPath;
   final bool isHollow;
   final double activePercent;
 
-  PagerBubbleViewModel({
-    this.color,
-    this.iconAssetPath,
-    this.isHollow,
-    this.activePercent
-  });
-
+  PageBubbleViewModel(
+      {this.color, this.iconAssetPath, this.isHollow, this.activePercent});
 }
